c0f23121fe5914a313da87a79d70bcf604f15993
evanwill <evanpeterw@gmail.com>
2016-03-05 02:13:09 -0800

    add basic template files.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # kioskSlides
-simple, offline, html + js slideshow for display computer
+
+> simple, offline, html + js slideshow for display computer
+
+Very basic slide show to replace PP on a display computer. HTML + JS allows interactive features in the slides.
+
+I attached an old receipt printer to the display computer to allow visitors to print poems from the slideshow. 
+
+## Set Up Chrome Kiosk Mode
+
+1. Set up printing defaults: 
+..* set default printer to receipt
+..* under more settings uncheck Headers and footers
+2. Create a new Kiosk user and shortcut
+..* settings > users > add new user > create shortcut for this user > create
+3. Edit the shortcut to add flags enabling kiosk mode and printing without a message 
+..* edit > properties > target
+..* target should = `chrome.exe --kiosk --kiosk-printing http://[start URL here] --disable-print-preview`
+4. Completely shut down Chrome (in task manager) or restart computer.
+
+Start the slideshow by clicking on the shortcut.
+
+To quit press Alt + F4 on keyboard (right click is disabled on the slide pages, so if keyboard is disconnected there is no way to exit the kiosk show).
+
+
---
+++ js/advance.js
@@ -0,0 +1,63 @@
+// kioskSlides slide advance script
+
+// array of slides. insert slide order here
+var slides = [ 
+    "fullSlide.html",
+    "poem.html",
+    "photoSlide.html"    
+    ];
+
+// get current slide
+var locate = window.location;
+
+// regex function to reduce location to file name
+function filename(slide) {
+        return slide == /\w+?\.html/.exec(locate);
+}
+
+// function to advance the slides in a loop
+function advance() {     
+    var nextSlide = slides.findIndex(filename) +1;
+    if (nextSlide === slides.length) {
+    window.location = slides[0];
+    } else {
+    window.location = slides[nextSlide];
+    } 
+}
+
+// function to advance to poem slide
+function getpoem() {
+    window.location = "poem.html";
+}
+
+// function to advance on click or timeout 
+// slide Option #1, call in body 
+function advanceTimeout() {
+    var t;
+    window.onload = resetTimer;
+    // option to advance the slide on a click 
+    // window.onclick = advance; 
+    // or jump to poem slide
+    window.onclick = getpoem;
+
+    function resetTimer() {
+        clearTimeout(t);
+        t = setTimeout(advance, 15000)
+        // 1000 milisec = 1 sec
+    }
+}
+
+// function to wait while mouse is active
+// slide Option #2, call in body
+function activeWait() {
+    var t2;
+    window.onload = resetTimer2;
+    document.onmousemove = resetTimer2;
+    document.onkeypress = resetTimer2;
+
+    function resetTimer2() {
+        clearTimeout(t2);
+        t2 = setTimeout(advance, 15000)
+        // 1000 milisec = 1 sec
+    }
+}
